0cf97d45413a1270a219ec89ff1bfd2a77fcf8c2
Md Shaad Rehan <113508620+mdshaad783@users.noreply.github.com>
2025-07-03 22:28:59 +0530

    Updated authMiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,23 +4,29 @@ import asyncHandler from './asyncHandler.js'
 
 const authenticate = asyncHandler(async(req,res,next)=>{
     let token;
-    token = req.cookies.jwt
-    if(token){
-        try {
-            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
-            req.user = await User.findById(decoded.userId).select('-password');
-            next()
-        } catch (error) {
-            res.status(401)
-            throw new Error("Not authorised,token failed...")   
-        }
-    }
-    else{
-        res.status(401)
-        throw new Error("Not authorised, no token...")
-    }
-})
+    // 1. Check Authorization header
+  if (req.headers.authorization?.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  // 2. Else fallback to cookie
+  else if (req.cookies?.jwt) {
+    token = req.cookies.jwt;
+  }
 
+   if (!token) {
+    res.status(401);
+    throw new Error("Not authorised, no token...");
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = await User.findById(decoded.userId).select('-password');
+    next();
+  } catch (error) {
+    res.status(401);
+    throw new Error("Not authorised, token failed...");
+  }
+});
 const authorizeAdmin = (req,res,next)=>{
     if (req.user && req.user.isAdmin){
         next()
